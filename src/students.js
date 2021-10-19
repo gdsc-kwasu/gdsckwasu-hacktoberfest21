a5e98b78b288e1246d7239ae070c988c2c730cde
Caleb <belac335@gmail.com>
2021-10-19 21:21:13 +0100

    feat: add base components

---
+++ src/students.js
@@ -0,0 +1,17 @@
+// this file will contain an array of students objects with
+// the properties that describes them.
+
+export const students = [
+  {
+    name: "Chief Clement",
+    avatar: "/img/avatar.jpg",
+    skills: "React, CSS, JavaScript",
+    gender: "Male",
+    interests: "Women, sports, money",
+    card_theme: "#4285f4",
+    student_id: 1,
+  },
+
+  // studenst can now begin to add their own details.
+  // We'll be mapping the items in this array into `pages/index.js`
+];
