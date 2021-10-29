3123f609626263e8a297c4cba3cdf20cc5add1b8
c-blaq <babayejuadesina@gmail.com>
2021-10-29 12:52:19 +0100

    added meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import '../styles/globals.css'
-import 'github-fork-ribbon-css/gh-fork-ribbon.css'
+import Head from "next/head";
+import Link from "next/link";
+import "../styles/globals.css";
+import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Students &mdash; Hacktoberfest</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Students Hacktoberfest" />
+        <meta property="og:url" content="https://devillary.netlify.app/" />
+        <meta
+          property="og:description"
+          content="This project is basically a student-developer profile viewer, 
+          where students can contribute by submitting a pull request with their profile details. 
+          It's being developed to give students a basic introduction to open source contribution 
+          in the period of hacktoberfest."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
@@ -19,7 +30,7 @@ function MyApp({ Component, pageProps }) {
         ></a>
       </Link>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
