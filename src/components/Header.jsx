725b51f561ab73f161326436d06a402163f44b53
Azubuike Nnamdi Clement <35131360+iamclement1@users.noreply.github.com>
2021-10-29 13:03:30 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import HeaderContainer from './styled/HeaderContainer.styled'
 export default function Header() {
   return (
     <HeaderContainer>
-      <h1>Devllary</h1>
+      <a href="https://devllary.netlify.app/"> <h1>Devllary</h1></a>
     </HeaderContainer>
   )
 }
